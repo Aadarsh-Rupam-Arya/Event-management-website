a7950e754d3bb1fdf6da9c499b7a1f9a7ec4648d
Aadarsh-Rupam-Arya <aadarsharya10@gmail.com>
2025-09-09 18:07:26 +0530

    new commit

--- server.js
+++ server.js
@@ -1,60 +1,51 @@
+require("dotenv").config();
 const express = require("express");
-const path = require("path"); 
+const path = require("path");
 const bodyParser = require("body-parser");
 const { MongoClient } = require("mongodb");
 
 const app = express();
-const port = 3010;
+const port = process.env.PORT || 3010;
 
-// const uri = "mongodb://localhost:27017";
-const uri = "mongodb+srv://aadarsharya10:3105Aadi%23@cluster0.q0ilc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-// 
-
-const dbName = "Magic_Moments";
+const uri = process.env.MONGO_URI;
+const dbName = process.env.DB_NAME;
 
 const collectionName = "users";
 
+app.use(express.static(path.join(__dirname, "static")));
+app.use(express.static(path.join(__dirname, "CSS")));
 
-
-app.use(express.static(path.join(__dirname,'static')));
-app.use(express.static(path.join(__dirname,'CSS')));
-
-
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,'/HTML/home.html'))
-})
-app.get("/home",(req,res)=>{
-    res.sendFile(path.join(__dirname,'/HTML/home.html'))
-})
-app.get("/details",(req,res)=>{
-    res.sendFile(path.join(__dirname,'/HTML/details.html'))
-})
-app.get("/mytickets",(req,res)=>{
-    res.sendFile(path.join(__dirname,'/HTML/mytickets.html'))
-})
-app.get("/orderstatus",(req,res)=>{
-    res.sendFile(path.join(__dirname,'/HTML/orderstatus1.html'))
-})
-app.get("/orderbill",(req,res)=>{
-    res.sendFile(path.join(__dirname,'/HTML/orderbill1.html'))
-})
-app.get("/payment",(req,res)=>{
-    res.sendFile(path.join(__dirname,'/HTML/payment.html'))
-})
-app.get("/user",(req,res)=>{
-    res.sendFile(path.join(__dirname,'/HTML/user.html'))
-})
-app.get("/new",(req,res)=>{
-  res.sendFile(path.join(__dirname,'/HTML/new.html'))
-})
-
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/HTML/home.html"));
+});
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "/HTML/home.html"));
+});
+app.get("/details", (req, res) => {
+  res.sendFile(path.join(__dirname, "/HTML/details.html"));
+});
+app.get("/mytickets", (req, res) => {
+  res.sendFile(path.join(__dirname, "/HTML/mytickets.html"));
+});
+app.get("/orderstatus", (req, res) => {
+  res.sendFile(path.join(__dirname, "/HTML/orderstatus1.html"));
+});
+app.get("/orderbill", (req, res) => {
+  res.sendFile(path.join(__dirname, "/HTML/orderbill1.html"));
+});
+app.get("/payment", (req, res) => {
+  res.sendFile(path.join(__dirname, "/HTML/payment.html"));
+});
+app.get("/user", (req, res) => {
+  res.sendFile(path.join(__dirname, "/HTML/user.html"));
+});
+app.get("/new", (req, res) => {
+  res.sendFile(path.join(__dirname, "/HTML/new.html"));
+});
 
 app.get("/profile", (req, res) => {
   res.sendFile(path.join(__dirname, "/HTML/profile.html"));
 });
-
-
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -78,7 +69,7 @@ app.post("/saveUserDetails", async (req, res) => {
 
   await saveUserDetails({ name, email, num, password });
   // res.sendFile(__dirname + "/NEW/password.html");
-  res.redirect('/user');
+  res.redirect("/user");
 });
 
 async function saveUserDetails(userDetails) {
@@ -113,11 +104,14 @@ async function saveUserDetails(userDetails) {
 
     if (user) {
       // res.redirect('/user');
-      res.send('<script>alert("You are successfully Login."); window.location.href = "/user";</script>');
-
+      res.send(
+        '<script>alert("You are successfully Login."); window.location.href = "/user";</script>'
+      );
     } else {
       // res.redirect('/login');
-       res.send('<script>alert("Invalid Email or Password! Please Try Again."); window.location.href = "/login";</script>');
+      res.send(
+        '<script>alert("Invalid Email or Password! Please Try Again."); window.location.href = "/login";</script>'
+      );
     }
   });
 
@@ -149,12 +143,13 @@ async function saveUserDetails(userDetails) {
   app.get("/feedback", (req, res) => {
     res.sendFile(path.join(__dirname, "/HTML/feedback.html"));
   });
- 
 
   app.post("/saveData", async (req, res) => {
     const { data } = req.body;
     await saveData({ data });
-    res.send('<script>alert("Your feedback is submited."); window.location.href = "/mytickets";</script>');
+    res.send(
+      '<script>alert("Your feedback is submited."); window.location.href = "/mytickets";</script>'
+    );
   });
 
   async function saveData(dataDetails) {
@@ -173,11 +168,9 @@ async function saveUserDetails(userDetails) {
   }
 }
 
-
 app.use((req, res) => {
-      res.status(404).send("<h1>404 - Not Found</h1>");
-  });
-  
+  res.status(404).send("<h1>404 - Not Found</h1>");
+});
 
 app.listen(port, () => {
   console.log(`Server is listening at http://127.0.0.1:${port}`);
